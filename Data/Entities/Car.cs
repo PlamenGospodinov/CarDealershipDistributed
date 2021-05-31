b3b634f74d85eb06635583c8e3e61d28b86fa12b
Пламен Господинов <plamko3@abv.bg>
2021-05-31 20:29:26 +0300

    Some data it the Car Entity

--- Data/Entities/Car.cs
+++ Data/Entities/Car.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,23 @@ namespace Data.Entities
 {
     public class Car : BaseEntity
     {
+        [Required]
+        [MaxLength(80)]
+        public string Model { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public string Condition { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Color { get; set; }
+
+        [Required]
+        public int Power { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(5, 2)")]
+        public decimal Price { get; set; }
     }
 }
