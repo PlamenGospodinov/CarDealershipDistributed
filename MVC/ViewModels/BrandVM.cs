78c597f8536beae0e8e7dc9165f934ac911fef39
Пламен Господинов <plamko3@abv.bg>
2021-06-05 14:07:57 +0300

    Created MVC Project.Deleted the WCF as it is not needed.Ready with BrandVM.

---
+++ MVC/ViewModels/BrandVM.cs
@@ -0,0 +1,39 @@
+﻿using ApplicationService.DTOs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC.ViewModels
+{
+    public class BrandVM
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string BrandName { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string CountryOfOrigin { get; set; }
+        public short FoundedIn { get; set; }
+        public DateTime? AddedOn { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string AddedFrom { get; set; }
+        public int LowestModelPrice { get; set; }
+
+        public BrandVM(){}
+
+        public BrandVM(BrandDTO brandDTO)
+        {
+            Id = brandDTO.Id;
+            BrandName = brandDTO.BrandName;
+            CountryOfOrigin = brandDTO.CountryOfOrigin;
+            FoundedIn = brandDTO.FoundedIn;
+            AddedOn = brandDTO.AddedOn;
+            AddedFrom = brandDTO.AddedFrom;
+            LowestModelPrice = brandDTO.LowestModelPrice;
+        }
+    }
+}
