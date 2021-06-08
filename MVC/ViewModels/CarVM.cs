1c29e2dabe53fc357c54841ec29a92dbcc0471c2
Пламен Господинов <plamko3@abv.bg>
2021-06-08 18:45:16 +0300

    Almost ready with data part.

--- MVC/ViewModels/CarVM.cs
+++ MVC/ViewModels/CarVM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ApplicationService.DTOs;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -8,6 +9,7 @@ namespace MVC.ViewModels
 {
     public class CarVM
     {
+        public int Id { get; set; }
         [Required]
         [StringLength(50, ErrorMessage = "Model name can't be that long.")]
         public string Model { get; set; }
@@ -37,5 +39,35 @@ namespace MVC.ViewModels
         [Required]
         [StringLength(50, ErrorMessage = "This field can't be that long.")]
         public string AddedBy { get; set; }
+
+        public int BrandId { get; set; }
+
+        public BrandVM BrandVM { get; set; }
+
+        public CarVM() { }
+
+        public CarVM(CarDTO carDto)
+        {
+            Id = carDto.Id;
+            Model = carDto.Model;
+            Condition = carDto.Condition;
+            Color = carDto.Color;
+            Power = carDto.Power;
+            Price = carDto.Price;
+            ManifactureDate = carDto.ManifactureDate;
+            Details = carDto.Details;
+            AddedBy = carDto.AddedBy;
+            BrandVM = new BrandVM
+            {
+                Id = carDto.BrandId,
+                BrandName = carDto.Brand.BrandName,
+                CountryOfOrigin = carDto.Brand.CountryOfOrigin,
+                FoundedIn = carDto.Brand.FoundedIn,
+                AddedOn = carDto.Brand.AddedOn,
+                AddedFrom = carDto.Brand.AddedFrom,
+                LowestModelPrice = carDto.Brand.LowestModelPrice
+
+            };
+        }
     }
 }
