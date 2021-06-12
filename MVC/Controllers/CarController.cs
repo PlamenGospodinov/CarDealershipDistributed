2c27198fd14d3be6bc7ffadf9a35742ab497d713
Пламен Господинов <plamko3@abv.bg>
2021-06-12 21:45:37 +0300

    Almost ready with CarController

--- MVC/Controllers/CarController.cs
+++ MVC/Controllers/CarController.cs
@@ -1,4 +1,5 @@
-﻿using MVC.ViewModels;
+﻿using MVC.Utils;
+using MVC.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,6 +22,120 @@ namespace MVC.Controllers
                 }
             }
             return View(carsVM);
+        }
+
+        // GET: Cars/Details/
+        public ActionResult Details(int id)
+        {
+            CarVM carVM = new CarVM();
+            using (ServiceReference1.Service1Client service = new ServiceReference1.Service1Client())
+            {
+                var carDto = service.GetCarByID(id);
+                carVM = new CarVM(carDto);
+            }
+
+            return View(carVM);
+        }
+
+        // GET: Cars/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cars/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(BrandVM brandVM)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (ServiceReference1.Service1Client service = new ServiceReference1.Service1Client())
+                    {
+
+                        
+                        //service.PostCar(carDTO);
+                    }
+
+                    return RedirectToAction("Index");
+                }
+
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        // GET: Cars/Edit
+
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+
+
+            CarVM carVM = new CarVM();
+            using (ServiceReference1.Service1Client service = new ServiceReference1.Service1Client())
+            {
+                
+                
+
+            }
+
+
+
+            ViewBag.Brands = LoadDataUtil.LoadBrandData();
+
+            return View(brandVM);
+        }
+
+        // POST: Cars/Edit/
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(BrandVM brandVM)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+
+                    using (ServiceReference1.Service1Client service = new ServiceReference1.Service1Client())
+                    {
+                        
+                       // service.PostCar(carDto);
+
+
+                    }
+
+
+
+                    return RedirectToAction("Index");
+                }
+
+                //ViewBag.Cars = LoadDataUtil.LoadCarData();
+                return View();
+            }
+            catch
+            {
+                //ViewBag.Cars = LoadDataUtil.LoadCarData();
+                return View();
+            }
+        }
+
+
+
+        public ActionResult Delete(int id)
+        {
+            using (ServiceReference1.Service1Client service = new ServiceReference1.Service1Client())
+            {
+                service.DeleteCar(id);
+            }
+            return RedirectToAction("Index");
         }
     }
 }
